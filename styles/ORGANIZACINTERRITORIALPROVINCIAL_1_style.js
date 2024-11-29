7d3f841a37aa0e0e06d4398ead223e6c38136d41
powerpaz <61800125+powerpaz@users.noreply.github.com>
2024-11-29 14:59:26 -0500

    Add files via upload

---
+++ styles/ORGANIZACINTERRITORIALPROVINCIAL_1_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_ORGANIZACINTERRITORIALPROVINCIAL_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'Rockwell Extra Bold\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("DPA_DESPRO") !== null) {
+        labelText = String(feature.get("DPA_DESPRO"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(228,26,28,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.3679999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
